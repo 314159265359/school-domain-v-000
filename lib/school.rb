ecba3cf6124d22b5c0787b2d129513528136dcba
Karen G <kgraves0701@gmail.com>
2018-03-22 20:03:53 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,14 +2,14 @@ class School
 
   def initialize(name)
     @name = name
-    @roster = {}
+    @roster = Hash.new{ |h, k| h[k] = [] }
 
   end
 
   attr_accessor :roster, :name, :grade
 
   def add_student(name, grade)
-    @roster[grade] = []
+    #@roster[grade] = []
     @roster[grade] << name
   end
 
