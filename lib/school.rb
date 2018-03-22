f48a5b02f893ba5ac33f085b15561dc0b78cc71e
Karen G <kgraves0701@gmail.com>
2018-03-22 20:00:36 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,13 +3,14 @@ class School
   def initialize(name)
     @name = name
     @roster = {}
-    
+
   end
 
   attr_accessor :roster, :name, :grade
 
   def add_student(name, grade)
-    @roster << [grade,name]
+    @roster[@grade] = @name
+    @roster[@grade]<< [grade,name]
   end
 
 end
